8f482d87b6b1b6058e7483973dfb32b891778efc
Stefani Fernanda Pereira Tosi <stefanitosi2@hotmail.comgit config --global user.namegit config --global user.email stefanitosi2@hotmail.com>
2021-06-19 16:02:36 -0300

    atualizado as medidas

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -9,6 +9,7 @@ export class HomePageComponent implements OnInit {
   public titulo='O que é saneamento básico?';
   public descricao='De acordo com a Lei 11.445/07, podemos definir como saneamento básico o conjunto de serviços, infraestruturas e instalações operacionais de abastecimento de água potável, esgotamento sanitário, limpeza urbana e manejo de resíduos sólidos e drenagem e manejo das águas pluviais urbanas, a falta de saneamento básico acarretam em varias doenças.';
   public logoUrl='https://www.eosconsultores.com.br/wp-content/uploads/2017/07/Quatro-servi%c3%a7os-do-plano-municipal-de-saneamento-b%c3%a1sico.png';
+
   constructor() { }
 
   ngOnInit(): void {
